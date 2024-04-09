09532f3afb2e9a7dd57ab1ea72bc52dd738704d5
Deepanshu <deepanshukhatri2468@gmail.com>
2024-04-10 01:10:50 +0530

    code-refactor

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -43,18 +43,18 @@ const signup_controller = async (req, res) => {
   }
 };
 const update_user = async (req, res) => {
-    try {
-      const user = await User.findOne({ where: { email: req.body.email } });
-  
-      if (user) {
-        await user.update(req.body); 
-        res.status(200).json(user);
-      } else {
-        res.status(404).json({ message: "User not found" });
-      }
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Internal Server Error" });
+  try {
+    const user = await User.findOne({ where: { email: req.body.email } });
+
+    if (user) {
+      await user.update(req.body);
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ message: "User not found" });
     }
-  };
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 module.exports = { signup_controller, update_user, login_controller };
--- utils/send_mail.js
+++ utils/send_mail.js
@@ -3,27 +3,24 @@ require("dotenv").config()
 // Function to send an email
 async function send_mail(email) {
     try {
-        // Create a transporter object using the default SMTP transport
         let transporter = nodemailer.createTransport({
             service: "gmail",
             host:'smtp.gmail.com',
             port: 587,
-            secure:false, // true for 465, false for other ports
+            secure:false, 
             auth: {
-                user: process.env.USER_EMAIL, // Your email
-                pass: process.env.APP_PASSWORD // Your password
+                user: process.env.USER_EMAIL, 
+                pass: process.env.APP_PASSWORD 
             }
         });
 
-        // Setup email data
         let mailOptions = {
-            from: `"Deepanshu Kumar" <${process.env.USER_EMAIL}>`, // sender address
-            to: `${email}`, // list of receivers
-            subject: 'Thankyou for Creating Account on Dribble', // Subject line
+            from: `"Deepanshu Kumar" <${process.env.USER_EMAIL}>`, 
+            to: `${email}`, 
+            subject: 'Thankyou for Creating Account on Dribble', 
             html: "<p>Dear User, Thankyou for creating an account with Dribble. We're thrilled to have you onboard!\n If you have any questions or need assistance, feel free to reach out to our support team.\n Happy designing!" // html body
         };
 
-        // Send mail with defined transport object
         let info = await transporter.sendMail(mailOptions);
         console.log('Message sent: %s', info.messageId);
         return info;
